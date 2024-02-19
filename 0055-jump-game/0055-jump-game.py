4330b23ebaa0d694000718a86f5340d6a1697d84
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-02-20 01:13:45 +0530

    Time: 372 ms (50.02%), Space: 17.8 MB (78.04%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,24 +1,24 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
 
-        # sol 1: going backward and checking if we can move 1 forward
-        goal = len(nums) - 1
+        # # sol 1: going backward and checking if we can move 1 forward
+        # goal = len(nums) - 1
 
-        for i in range(len(nums) - 1, -1, -1):
+        # for i in range(len(nums) - 1, -1, -1):
 
-            if i + nums[i] >= goal:
-                goal = i
+        #     if i + nums[i] >= goal:
+        #         goal = i
         
-        return goal == 0
+        # return goal == 0
 
-        # # sol 2
-        # reachable = 0
+        # sol 2
+        reachable = 0
 
-        # for i in range(len(nums)):
+        for i in range(len(nums)):
 
-        #     if reachable < i:
-        #         return False
+            if reachable < i:
+                return False
             
-        #     reachable = max(reachable, i + nums[i])
+            reachable = max(reachable, i + nums[i])
         
-        # return True
+        return True
