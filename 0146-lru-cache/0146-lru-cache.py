dc85c3238e0201f570088c940f2e5d0efb6c2554
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-02-25 03:21:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,30 +1,71 @@
-from collections import OrderedDict
-
+class doubly:
+    
+    def __init__(self, key, value):
+        self.key = key
+        self.val = value
+        self.next = None
+        self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = OrderedDict()
         self.capacity = capacity
+        # we use hmap for contant lookup time
+        self.hmap = {} 
+        # we use linkedlist for contant insertion and deletion
+        self.head = doubly(0, 0) 
+        self.tail = doubly(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-        else:
-            # Move the key to the end to show that it was recently used
-            value = self.cache.pop(key)
-            self.cache[key] = value  # reinsert item to move it to the end
-            return value
+        
+        if key in self.hmap:
+            node = self.hmap[key]
+
+            node.prev.next = node.next
+            node.next.prev = node.prev
+
+            nextval = self.head.next
+            self.head.next = node
+            node.prev = self.head
+            node.next = nextval
+            nextval.prev = node
+
+            return node.val
+
+        return -1    
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            # Remove existing key before reinserting it
-            self.cache.pop(key)
-        elif len(self.cache) >= self.capacity:
-            # popitem(last=False) pops the first item inserted if last is False
-            self.cache.popitem(last=False)
-        self.cache[key] = value
 
+        if key in self.hmap:
+            node = self.hmap[key]
+
+            node.prev.next = node.next
+            node.next.prev = node.prev
+
+            nextval = self.head.next
+            self.head.next = node
+            node.prev = self.head
+            node.next = nextval
+            nextval.prev = node
+
+            node.val = value
+        
+        elif len(self.hmap) >= self.capacity:
+
+            node = self.tail.prev
+            del self.hmap[node.key]
+            self.tail.prev = self.tail.prev.prev
+        
+        node = doubly(key, value)
+        self.hmap[key] = node
+
+        nextval = self.head.next
+        self.head.next = node
+        node.prev = self.head
+        node.next = nextval
+        nextval.prev = node
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
