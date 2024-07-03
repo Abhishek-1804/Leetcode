4fafd069d9fc311a25a6b5f2b5cebc4124f47e49
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-07-02 19:02:35 -0700

    Time: 78 ms (99.41%), Space: 16.6 MB (57.58%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.py
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -1,32 +1,32 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        one_d_board = [0]
+
         n = len(board)
-        
-        for row in range(len(board)-1, -1, -1):
-            if (n - 1 - row) % 2 == 0:
-                for col in range(len(board)):
-                    one_d_board.append(board[row][col])
-            else:
-                for col in range(len(board)-1, -1, -1):
-                    one_d_board.append(board[row][col])
-        
+        board.reverse()
+
+        def intToPos(square):
+            r, c = (square - 1) // n, (square - 1) % n
+            if r % 2:
+                c = n - 1 - c
+            return (r, c)
+
         q = collections.deque([(1,0)])
-        visit = set([1])
+        visit = set()
         while q:
-            index, moves = q.popleft()
+            square, moves = q.popleft()
             
             for inc in range(1,7):
-                nextSquare = index + inc
-                if nextSquare > n * n:
-                    break
-            
-                square_val = one_d_board[nextSquare]
+                nextSquare = square + inc
+
+                r,c = intToPos(nextSquare)
+                square_val = board[r][c]
+
                 if square_val != -1:
                     nextSquare = square_val
         
                 if nextSquare == n * n:
                     return moves + 1
+
                 if nextSquare not in visit:
                     visit.add(nextSquare)
                     q.append((nextSquare, moves + 1))
