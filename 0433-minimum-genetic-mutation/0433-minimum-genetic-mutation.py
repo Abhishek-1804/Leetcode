1d34b1b381eb79dd7ecc1fb420dfcbb4084df850
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-07-05 14:23:38 -0700

    Time: 42 ms (12.26%), Space: 16.5 MB (29.51%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -12,15 +12,14 @@ class Solution:
             
             # Generate mutations by only changing characters that differ from the endGene
             for i in range(len(curr_gene)):
-                if curr_gene[i] != endGene[i]:
-                    for char in gene_chars:
-                        if curr_gene[i] != char:
-                            new_gene = curr_gene[:i] + char + curr_gene[i+1:]
-                            
-                            if new_gene in bank_set:
-                                if new_gene == endGene:
-                                    return mutations+1
-                                q.append((new_gene, mutations + 1))
-                                bank_set.remove(new_gene)  # Avoid re-processing
+                for char in gene_chars:
+                    if curr_gene[i] != char:
+                        new_gene = curr_gene[:i] + char + curr_gene[i+1:]
+                        
+                        if new_gene in bank_set:
+                            if new_gene == endGene:
+                                return mutations+1
+                            q.append((new_gene, mutations + 1))
+                            bank_set.remove(new_gene)  # Avoid re-processing
             
         return -1
