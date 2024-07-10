e1fb22b5885c2d9ea1e6d93866f43e5684d6b4be
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-07-10 11:53:27 -0700

    Time: 269 ms (66.96%), Space: 32.5 MB (24.56%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -5,47 +5,36 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
+        # Base Case: If the length of the linked list is less than or equal to 1, then the list is already sorted
         if not head or not head.next:
             return head
-        
-        left = head
-        right = self.getMid(head)
-        temp = right.next
-        right.next = None
-        right = temp
 
-        left = self.sortList(left)
-        right = self.sortList(right)
-        
-        return self.merge(left, right)
-    
-    def getMid(self, head):
+        # Split the linked list into two halves using "slow and fast pointer" technique to find the midpoint of the linked list
         slow, fast = head, head.next
-
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        
-        return slow
-    
-    def merge(self, left, right):
-        head = dummy = ListNode()
+        # The midpoint of the linked list is slow.next
+        mid = slow.next
+        # Set slow.next to None to separate the left and right halves of the linked list
+        slow.next = None
 
+        # Recursively sort the left and right halves of the linked list
+        left = self.sortList(head)
+        right = self.sortList(mid)
+
+        # Merge the two sorted halves of the linked list
+        dummy = ListNode(0)
+        curr = dummy
         while left and right:
             if left.val < right.val:
-                head.next = left
+                curr.next = left
                 left = left.next
-            
             else:
-                head.next = right
+                curr.next = right
                 right = right.next
-            head = head.next
-        
-        if left:
-            head.next = left
-        
-        if right:
-            head.next = right
-        
+            curr = curr.next
+        # Append the remaining nodes of the left or right half to the end of the sorted list
+        curr.next = left or right
+
         return dummy.next
