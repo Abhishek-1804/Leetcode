cdf2177e358d91371570829e3f7e1306c5181e7c
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-07-20 04:29:22 +0530

    Time: 605 ms (9.45%), Space: 33.4 MB (7.55%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,56 +1,9 @@
-class MaxHeap:
-    def __init__(self):
-        self.heap = []
-    
-    def heapify_up(self, index):
-        parent = (index-1) // 2
-        
-        if index > 0 and self.heap[parent] < self.heap[index]:
-            self.heap[parent], self.heap[index] = self.heap[index], self.heap[parent]
-            self.heapify_up(parent)
-    
-    def heapify_down(self, index):
-        largest = index
-        left = 2 * index + 1
-        right = 2 * index + 2
-
-        if left < len(self.heap) and self.heap[left] > self.heap[largest]:
-            largest = left
-        
-        if right < len(self.heap) and self.heap[right] > self.heap[largest]:
-            largest = right
-        
-        if largest != index:
-            self.heap[largest], self.heap[index] = self.heap[index], self.heap[largest]
-            self.heapify_down(largest)
-    
-    def insert(self, val):
-        self.heap.append(val)
-        self.heapify_up(len(self.heap) - 1)
-    
-    def extract_max(self):
-        if not self.heap:
-            return None
-        if len(self.heap) == 1:
-            return self.heap.pop()
-        
-        root = self.heap[0]
-        self.heap[0] = self.heap.pop()
-        self.heapify_down(0)
-        return root
-
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
 
-        maxHeap = MaxHeap()
-
-        for i in nums:
-            maxHeap.insert(i)
-        
-        for _ in range(k-1):
-            maxHeap.extract_max()
-        
-        return maxHeap.extract_max()
+        heapq.heapify(nums)
+        largest = heapq.nlargest(k, nums)
+        return largest.pop()
 
         ## QuickSelect Solution (similar to quicksort; but fails for 1 test case)
         # k = len(nums) - k
