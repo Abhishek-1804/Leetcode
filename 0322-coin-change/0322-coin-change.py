1031a9ac9aa68113d054178795eeab28df8e3c5e
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-07-23 23:19:04 +0530

    Time: 763 ms (68.42%), Space: 17 MB (49.6%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,28 +1,12 @@
 class Solution:
-    def coinChange(self, arr: List[int], sum_: int) -> int:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        dp = [amount+1] * (amount + 1)
+        dp[0] = 0
+
+        for a in range(1, amount+1):
+            for c in coins:
+                if a-c >= 0:
+                    dp[a] = min(dp[a], 1+dp[a-c])
         
-        d = [[-1 for i in range(sum_ + 1)] for j in range(len(arr) + 1)]
-
-        for i in range(len(d[0])):
-            d[0][i] = float('inf')
-
-        for i in range(1, len(d)):
-            d[i][0] = 0
-
-        for i in range(1, len(d[0])):
-            if i % arr[0] == 0:
-                d[1][i] = i // arr[0]
-
-            else:
-                d[1][i] = float('inf')
-
-        for i in range(2, len(d)):
-            for j in range(1, len(d[0])):
-
-                if j >= arr[i-1]:
-                    d[i][j] = min(d[i][j-arr[i-1]] + 1, d[i-1][j])
-
-                else:
-                    d[i][j] = d[i-1][j]
-
-        return -1 if d[-1][-1] == float('inf') else d[-1][-1]
+        return dp[amount] if dp[amount] != amount + 1 else -1
