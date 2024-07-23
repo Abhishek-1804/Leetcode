d0220e6fc1de04045aab618435a1d2227c291603
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-07-23 07:15:07 +0530

    Time: 38 ms (57.82%), Space: 16.5 MB (61.49%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -2,8 +2,6 @@ class Solution:
     def trailingZeroes(self, n: int) -> int:
         
         result = 0
-        if n < 5:
-            return result
 
         while n > 0:
             result += n//5
