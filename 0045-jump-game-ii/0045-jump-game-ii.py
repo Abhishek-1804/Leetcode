cb98261763be97ad1ded17042322107ba8703bd0
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-08-02 04:27:42 +0530

    Time: 98 ms (89.02%), Space: 17.8 MB (10.13%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,24 +1,19 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
 
-        counter = 0
-        
-        if len(nums) == 1:
+        if len(nums) <= 1:
             return 0
+        
+        end = 0
+        jump = 0
+        farthest = 0
 
-        q = deque([(0, 0)])
-        visited = set()
-
-        while q:
-            ind, count = q.popleft()
-            if ind == len(nums)-1:
-                return count
-            
-            if nums[ind] == 0:
-                continue
-
-            for j in range(1, nums[ind] + 1):
-                jump = j + ind
-                if jump < len(nums) and jump not in visited:
-                    q.append((jump, count+1))
-                    visited.add(jump)
+        for i in range(len(nums)):
+            farthest = max(farthest, i + nums[i])
+            if farthest >= len(nums)-1:
+                return jump + 1
+            if i == end:
+                jump += 1
+                end = farthest
+        
+        return jump
