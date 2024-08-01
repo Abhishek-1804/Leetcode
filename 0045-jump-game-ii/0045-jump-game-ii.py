84d3fc946ec4231ee5afb08399bea9b475efd290
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-08-02 00:08:18 +0530

    Time: 1990 ms (21.63%), Space: 18.5 MB (6.71%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -6,23 +6,19 @@ class Solution:
         if len(nums) == 1:
             return 0
 
-        q = deque([0])
+        q = deque([(0, 0)])
         visited = set()
 
         while q:
-            l = len(q)
-            for _ in range(l):
-                ind = q.popleft()
-                if ind == len(nums)-1:
-                    return counter
-                
-                if nums[ind] == 0:
-                    continue
-
-                for j in range(1, nums[ind] + 1):
-                    jump = j + ind
-                    if jump < len(nums) and jump not in visited:
-                        q.append(jump)
-                        visited.add(jump)
+            ind, count = q.popleft()
+            if ind == len(nums)-1:
+                return count
             
-            counter += 1
+            if nums[ind] == 0:
+                continue
+
+            for j in range(1, nums[ind] + 1):
+                jump = j + ind
+                if jump < len(nums) and jump not in visited:
+                    q.append((jump, count+1))
+                    visited.add(jump)
