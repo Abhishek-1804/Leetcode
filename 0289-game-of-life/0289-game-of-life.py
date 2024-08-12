9bea9df6aa882c5da95e77506c03638569791f55
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-08-13 04:09:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.py
+++ 0289-game-of-life/0289-game-of-life.py
@@ -3,13 +3,12 @@ class Solution:
         # returns number of 1's around the cell
 
         dir = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
-
         count = 0
         for r, c in dir:
             new_row = row+r
             new_col = col+c
-            if new_row >= 0 and new_col >= 0 and new_row < len(board) and new_col < len(board[0]):
-                count += board[new_row][new_col][0]
+            if new_row >= 0 and new_col >= 0 and new_row < len(board) and new_col < len(board[0]) and abs(board[new_row][new_col]) == 1:
+                count += 1
         
         return count
 
@@ -17,31 +16,20 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-
-        # add booleans to values
         for row in range(len(board)):
             for col in range(len(board[0])):
-                if board[row][col] == 1:
-                    board[row][col] = [board[row][col], True]
-                elif board[row][col] == 0:
-                    board[row][col] = [board[row][col], False]
-        
-        # change booleans
-        for row in range(len(board)):
-            for col in range(len(board[0])):
-                if board[row][col][0] == 0:
-                    if self.helper(board, row, col) == 3:
-                        board[row][col][1] = True
+                live_neighbors = self.helper(board, row, col)
                 
-                else:
-                    if self.helper(board, row, col) < 2 or self.helper(board, row, col) > 3:
-                        board[row][col][1] = False
+                if self.helper(board, row, col) < 2 or self.helper(board, row, col) > 3:
+                    board[row][col] = -1
+
+                if board[row][col] == 0 and live_neighbors == 3:
+                    board[row][col] = 2
         
-        # change values according to booleans
         for row in range(len(board)):
             for col in range(len(board[0])):
-                if board[row][col][1] == True:
+                if board[row][col] == 2:
                     board[row][col] = 1
-                else:
+                elif board[row][col] == -1:
                     board[row][col] = 0
         
