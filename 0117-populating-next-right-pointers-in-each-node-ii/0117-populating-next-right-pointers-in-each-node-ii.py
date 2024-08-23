392660d5bef6f5d3508260a323fd939872b5932d
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-08-24 04:56:49 +0530

    Time: 43 ms (57.63%), Space: 17.7 MB (43.46%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -10,58 +10,22 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-
-        # sol2
-        def dfs(node):
-            
-            if not node:
-                return None
-
-            scanner = node.next
-
-            while scanner:
-
-                if scanner.left:
-                    scanner = scanner.left
-                    break
-                
-                if scanner.right:
-                    scanner = scanner.right
-                    break
-                
-                scanner = scanner.next
-            
-            if node.right:
-                node.right.next = scanner
-            
-            if node.left:
-                node.left.next = node.right if node.right else scanner
-            
-            dfs(node.right)
-            dfs(node.left)
-
-            return node
         
-        return dfs(root)
-
-        ## sol 1 bfs approach
-        # if not root:
-        #     return None
-            
-        # queue = [root]
-
-        # while queue:
-        #     size = len(queue)
-            
-        #     for i in range(size):
-        #         node = queue.pop(0)
-        #         if i < size - 1:
-        #             node.next = queue[0]
-                
-        #         # Add children to the queue
-        #         if node.left:
-        #             queue.append(node.left)
-        #         if node.right:
-        #             queue.append(node.right)
+        if not root:
+            return
         
-        # return root
+        q = collections.deque([root])
+        
+        while q:
+            n = len(q)
+            temp = None
+            for i in range(n):
+                node = q.pop()
+                node.next = temp
+                temp = node
+                if node.right:
+                    q.appendleft(node.right)
+                if node.left:
+                    q.appendleft(node.left)
+
+        return root
