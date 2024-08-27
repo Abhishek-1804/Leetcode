cb598d3e05cef6e1e6fa230e686c3c06d066e8a7
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-08-28 04:19:05 +0530

    Time: 263 ms (36.58%), Space: 24 MB (38.06%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,31 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
-        if not grid:
-            return 0
-
-        visited = set()
-        islands = 0
-
-        def bfs(row, col):
-            queue = deque([(row, col)])
-            while queue:
-                r, c = queue.popleft()
-                if (r, c) not in visited:
-                    visited.add((r, c))
-                    # Check all four directions
-                    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-                    for dr, dc in directions:
-                        nr, nc = r + dr, c + dc
-                        if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and (nr, nc) not in visited and grid[nr][nc] == "1":
-                            queue.append((nr, nc))
+        
+        seen = set()
+        counter = 0
 
         for row in range(len(grid)):
             for col in range(len(grid[0])):
-                if grid[row][col] == "1" and (row, col) not in visited:
-                    bfs(row, col)
-                    islands += 1
+                if (row, col) in seen or grid[row][col] == '0':
+                    continue
+                else:
+                    q = collections.deque([(row, col)])
+                    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+                    while q:
+                        r, c = q.popleft()
+                        for nr, nc in directions:
+                            new_row = r + nr
+                            new_col = c + nc
+                            if (new_row, new_col) in seen:
+                                continue
+                            if new_row >= 0 and new_row < len(grid) and new_col >= 0 and new_col < len(grid[0]) and grid[new_row][new_col] == '1':
+                                q.append((new_row, new_col))
+                                seen.add((new_row, new_col))
+                    counter += 1
         
-        return islands
-    
-    
+        return counter
