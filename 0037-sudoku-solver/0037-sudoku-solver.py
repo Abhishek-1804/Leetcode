34d47e392e1739b2285f9c029edb2570b8936974
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-09-03 04:38:51 +0530

    Time: 395 ms (8.75%), Space: 16.5 MB (93.9%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,39 +1,36 @@
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
-        def is_valid(board, row, col, num):
-            # Check if the number exists in the row
-            for i in range(9):
-                if board[row][i] == num:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        def is_valid(row, col, num):
+            for i in range(0, 9):
+                if board[row][i] == str(num):
+                    return False
+                if board[i][col] == str(num):
                     return False
             
-            # Check if the number exists in the column
-            for i in range(9):
-                if board[i][col] == num:
-                    return False
-            
-            # Check if the number exists in the 3x3 subgrid
             start_row = 3 * (row // 3)
             start_col = 3 * (col // 3)
             for i in range(3):
                 for j in range(3):
-                    if board[start_row + i][start_col + j] == num:
+                    if board[start_row + i][start_col + j] == str(num):
                         return False
             
             return True
 
         def backtrack(board):
-            for row in range(9):
-                for col in range(9):
+            for row in range(len(board)):
+                for col in range(len(board)):
                     if board[row][col] == '.':
-                        for num in map(str, range(1, 10)):
-                            if is_valid(board, row, col, num):
-                                board[row][col] = num
-                                if backtrack(board):  # If the board is solvable from here, return True
+                        for i in range(1, 10):
+                            if is_valid(row, col, i):
+                                board[row][col] = str(i)
+                                if backtrack(board):
                                     return True
-                                board[row][col] = '.'  # Otherwise, undo the placement (backtrack)
-                        return False  # No valid number found, so return False
-            return True  # Solved
+                                board[row][col] = '.'
+                        return False
+            return True
         
-        backtrack(board)
-        return board
-        
+        if backtrack(board):
+            return board
