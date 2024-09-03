1fb0db6db07a57849bd6e02ab4f96d01a15a18a1
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-09-03 06:11:03 +0530

    Time: 282 ms (56.83%), Space: 32.4 MB (69.76%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -5,33 +5,34 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-
+        
         if not head or not head.next:
             return head
-
-        slow = head
-        fast = head.next
+        
+        #floyds tortoise and hare to find midpoint
+        slow, fast = head, head.next
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
         
+        #setting left and right pointers
         mid = slow.next
         slow.next = None
 
         left = self.sortList(head)
         right = self.sortList(mid)
 
-        curr = dummy = ListNode()
+        dummy = curr = ListNode()
 
         while left and right:
             if left.val <= right.val:
                 curr.next = left
                 left = left.next
-            
             else:
                 curr.next = right
                 right = right.next
             curr = curr.next
         
         curr.next = left or right
+
         return dummy.next
