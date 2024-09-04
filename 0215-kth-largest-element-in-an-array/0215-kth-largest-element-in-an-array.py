f7af9b78680a5b63b6cc82f2d0b421035da8b1e6
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-09-04 06:22:42 +0530

    Time: 1085 ms (5.01%), Space: 29.7 MB (31.96%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,35 +1,84 @@
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+    
+    def _heapify_down(self, index):
+        left = 2 * index + 1
+        right = 2 * index + 2
+        smallest = index
+
+        if left < len(self.heap) and self.heap[left] < self.heap[smallest]:
+            smallest = left
+        if right < len(self.heap) and self.heap[right] < self.heap[smallest]:
+            smallest = right
+        
+        if smallest != index:
+            self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
+            self._heapify_down(smallest)
+
+    def _heapify_up(self, index):
+        parent = (index-1) // 2
+        if index > 0 and parent >= 0 and self.heap[index] < self.heap[parent]:
+            self.heap[index], self.heap[parent] = self.heap[parent], self.heap[index]
+            self._heapify_up(parent)
+    
+    def _heap_insert(self, val):
+        self.heap.append(val)
+        self._heapify_up(len(self.heap)-1)
+    
+    def _heap_pop(self):
+        if not self.heap:
+            return
+        root = self.heap[0]
+        self.heap[0] = self.heap[-1]
+        self.heap.pop()
+        self._heapify_down(0)
+        return root
+
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+    
+    def _heapify_down(self, index):
+        left = 2 * index + 1
+        right = 2 * index + 2
+        largest = index
+
+        if left < len(self.heap) and self.heap[left] > self.heap[largest]:
+            largest = left
+        if right < len(self.heap) and self.heap[right] > self.heap[largest]:
+            largest = right
+        
+        if largest != index:
+            self.heap[index], self.heap[largest] = self.heap[largest], self.heap[index]
+            self._heapify_down(largest)
+
+    def _heapify_up(self, index):
+        parent = (index-1) // 2
+        if index > 0 and parent >= 0 and self.heap[index] > self.heap[parent]:
+            self.heap[index], self.heap[parent] = self.heap[parent], self.heap[index]
+            self._heapify_up(parent)
+    
+    def heap_insert(self, val):
+        self.heap.append(val)
+        self._heapify_up(len(self.heap)-1)
+    
+    def heap_pop(self):
+        if not self.heap:
+            return
+        root = self.heap[0]
+        self.heap[0] = self.heap[-1]
+        self.heap.pop()
+        self._heapify_down(0)
+        return root
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        h = MaxHeap()
+        for num in nums:
+            h.heap_insert(num)
 
-        heap = nums[:k]
-        heapq.heapify(heap)
+        for _ in range(k-1):
+            h.heap_pop()
         
-        for num in nums[k:]:
-            if num > heap[0]:
-                heapq.heappop(heap)
-                heapq.heappush(heap, num)
-        
-        return heap[0]
-
-        ## QuickSelect Solution (similar to quicksort; but fails for 1 test case)
-        # k = len(nums) - k
- 
-        # def quickSelect(l, r):
-            # pivot, pointer = nums[r], l
- 
-            # for i in range(l, r):
-                # if nums[i] <= pivot:
-                    # nums[i], nums[pointer] = nums[pointer], nums[i]
-                    # pointer += 1
-            # nums[pointer], nums[r] = nums[r], nums[pointer]
- 
-            # if pointer < k:
-                # return quickSelect(pointer + 1, r)
-             
-            # elif pointer > k:
-                # return quickSelect(l, pointer - 1)
-             
-            # else:
-                # return nums[pointer]
-         
-        # return quickSelect(0, len(nums)-1)
+        return h.heap_pop()
