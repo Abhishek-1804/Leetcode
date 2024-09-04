0e1a2b99fe85001aae1278713fc87eae70412d93
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-09-04 05:38:16 +0530

    Time: 486 ms (41.97%), Space: 29.6 MB (69.29%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,35 +1,12 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        import heapq
 
-        heap = nums[:k]
-        heapq.heapify(heap)
-        
-        for num in nums[k:]:
-            if num > heap[0]:
-                heapq.heappop(heap)
-                heapq.heappush(heap, num)
-        
-        return heap[0]
+        nums = [-i for i in nums]
 
-        ## QuickSelect Solution (similar to quicksort; but fails for 1 test case)
-        # k = len(nums) - k
- 
-        # def quickSelect(l, r):
-            # pivot, pointer = nums[r], l
- 
-            # for i in range(l, r):
-                # if nums[i] <= pivot:
-                    # nums[i], nums[pointer] = nums[pointer], nums[i]
-                    # pointer += 1
-            # nums[pointer], nums[r] = nums[r], nums[pointer]
- 
-            # if pointer < k:
-                # return quickSelect(pointer + 1, r)
-             
-            # elif pointer > k:
-                # return quickSelect(l, pointer - 1)
-             
-            # else:
-                # return nums[pointer]
-         
-        # return quickSelect(0, len(nums)-1)
+        heapq.heapify(nums)
+
+        for _ in range(k-1):
+            heapq.heappop(nums)
+        
+        return -heapq.heappop(nums)
