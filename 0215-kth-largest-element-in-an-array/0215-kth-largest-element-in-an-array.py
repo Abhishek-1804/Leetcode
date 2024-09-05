32f290ca64b8260e5b5b937d93c7d95a20d97c02
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-09-06 00:44:18 +0530

    Time: 1098 ms (5.01%), Space: 29.6 MB (69.39%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,84 +1,48 @@
 class MinHeap:
+
     def __init__(self):
         self.heap = []
     
+    def _heapify_up(self, index):
+        parent = (index - 1) // 2
+        if index > 0 and parent >= 0 and self.heap[index] < self.heap[parent]:
+            self.heap[index], self.heap[parent] = self.heap[parent], self.heap[index]
+            self._heapify_up(parent)
+
     def _heapify_down(self, index):
-        left = 2 * index + 1
-        right = 2 * index + 2
+        left = 2*index + 1
+        right = 2*index + 2
         smallest = index
 
         if left < len(self.heap) and self.heap[left] < self.heap[smallest]:
             smallest = left
+
         if right < len(self.heap) and self.heap[right] < self.heap[smallest]:
             smallest = right
         
         if smallest != index:
             self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
             self._heapify_down(smallest)
-
-    def _heapify_up(self, index):
-        parent = (index-1) // 2
-        if index > 0 and parent >= 0 and self.heap[index] < self.heap[parent]:
-            self.heap[index], self.heap[parent] = self.heap[parent], self.heap[index]
-            self._heapify_up(parent)
-    
-    def _heap_insert(self, val):
-        self.heap.append(val)
-        self._heapify_up(len(self.heap)-1)
-    
-    def _heap_pop(self):
-        if not self.heap:
-            return
-        root = self.heap[0]
-        self.heap[0] = self.heap[-1]
-        self.heap.pop()
-        self._heapify_down(0)
-        return root
-
-class MaxHeap:
-    def __init__(self):
-        self.heap = []
-    
-    def _heapify_down(self, index):
-        left = 2 * index + 1
-        right = 2 * index + 2
-        largest = index
-
-        if left < len(self.heap) and self.heap[left] > self.heap[largest]:
-            largest = left
-        if right < len(self.heap) and self.heap[right] > self.heap[largest]:
-            largest = right
-        
-        if largest != index:
-            self.heap[index], self.heap[largest] = self.heap[largest], self.heap[index]
-            self._heapify_down(largest)
-
-    def _heapify_up(self, index):
-        parent = (index-1) // 2
-        if index > 0 and parent >= 0 and self.heap[index] > self.heap[parent]:
-            self.heap[index], self.heap[parent] = self.heap[parent], self.heap[index]
-            self._heapify_up(parent)
     
     def heap_insert(self, val):
         self.heap.append(val)
         self._heapify_up(len(self.heap)-1)
     
     def heap_pop(self):
-        if not self.heap:
-            return
-        root = self.heap[0]
+        top_val = self.heap[0]
         self.heap[0] = self.heap[-1]
         self.heap.pop()
         self._heapify_down(0)
-        return root
+        return top_val
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        h = MaxHeap()
+        
+        h = MinHeap()
         for num in nums:
-            h.heap_insert(num)
-
+            h.heap_insert(-num)
+        
         for _ in range(k-1):
             h.heap_pop()
         
-        return h.heap_pop()
+        return -h.heap_pop()
