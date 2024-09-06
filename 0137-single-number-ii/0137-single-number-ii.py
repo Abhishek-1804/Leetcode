0b4f17f349e5a877d4a87954f5d62922866aca04
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-09-07 00:03:37 +0530

    Time: 90 ms (21.94%), Space: 18.6 MB (76.65%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,18 +1,19 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
 
-        bits = [0]*32
+        bits_count = [0] * 32
 
         for num in nums:
             for i in range(32):
-                bits[i] += num & (1 << i)
-        
+                if num & (1 << i):
+                    bits_count[i] += 1
+
         result = 0
         for i in range(32):
-            if bits[i]%3 != 0:
-                result |= (1<<i)
-        
-        if bits[31]%3 != 0:
-            result -= 1 << 32
-        
+            if bits_count[i] % 3 != 0:
+                if i == 31:  # Handle the sign bit for negative numbers
+                    result -= (1 << i)
+                else:
+                    result |= (1 << i)
+
         return result
