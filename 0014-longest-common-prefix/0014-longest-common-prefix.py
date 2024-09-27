e063f272c996dc91f112733f719ba3cb242e8df1
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-09-28 00:50:27 +0530

    Time: 38 ms (54.44%), Space: 16.7 MB (32.89%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,32 @@
+class Trie:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        dummy = Trie()
+        head = dummy
+
+        for c in strs[0]:
+            head.children[c] = Trie()
+            head = head.children[c]
+        head.endOfWord = True
+
+        ans = strs[0]
+
+        for word in strs[1:]:
+            head = dummy
+            length_ = 0
+
+            for c in word:
+                if c not in head.children:
+                    break
+                head = head.children[c]
+                length_ += 1
+            
+            ans = ans[:length_]
+            if not length_:
+                return ""
+                
+        return ans
