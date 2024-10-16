8b7291b5dd00f8a8fbfb53d0cca3f4dd17253dbd
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-10-16 13:08:41 -0700

    Time: 101 ms (21.16%), Space: 18.3 MB (59.86%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,5 +1,5 @@
 class Solution:
-    def minRemoveToMakeValid(self, s: str) -> str:
+    def minRemoveToMakeValid_2(self, s: str) -> str:
 
         d = {}
         stack = []
@@ -27,3 +27,31 @@ class Solution:
             ans += val
         
         return ans
+
+    def minRemoveToMakeValid(self, s: str) -> str:
+
+        a = ''
+        open_count = 0
+
+        for i in range(len(s)):
+            if s[i].isalpha():
+                a += s[i]
+            else:
+                if s[i] == ')':
+                    if open_count:
+                        a += s[i]
+                        open_count -= 1
+                    else:
+                        continue
+                else:
+                    open_count += 1
+                    a += s[i]
+
+        final_ans = ''
+        for i in a[::-1]:
+            if open_count and i == '(':
+                open_count -= 1
+            else:
+                final_ans += i
+        
+        return final_ans[::-1]
