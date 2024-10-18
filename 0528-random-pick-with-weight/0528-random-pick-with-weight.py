051a7bec9b164eb65f659f7bb43d48f64087dbab
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-10-18 13:10:41 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -0,0 +1,26 @@
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.w = w
+        for i in range(1, len(self.w)):
+            self.w[i] += self.w[i-1]
+
+    def pickIndex(self) -> int:
+        import random
+        randNum = random.randint(1, self.w[-1])
+        
+        start, end = 0, len(self.w)-1
+        while start < end:
+            mid = (start+end) // 2
+            if self.w[mid] < randNum:
+                end = mid
+            else:
+                start = mid + 1
+        
+        return start
+
+        
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
