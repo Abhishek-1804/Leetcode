868af809b9f5093b6277d6fdae3d6fdb7c0a61e9
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-10-20 16:09:14 -0700

    Time: 19 ms (99.99%), Space: 19.5 MB (66.05%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,8 +1,7 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         cum_sum = 0
-        sum_count = collections.defaultdict(int)
-        sum_count[0] = 1  # Initialize with 0 sum occurring once
+        sum_count = {0:1}
         ans = 0
 
         for num in nums:
@@ -10,9 +9,12 @@ class Solution:
 
             # Check if there is a subarray (ending at the current index) that sums to k
             if (cum_sum - k) in sum_count:
-                ans += 1
+                ans += sum_count[cum_sum - k]
             
             # Update the count of the current cumulative sum
-            sum_count[cum_sum] += 1
+            if cum_sum not in sum_count:
+                sum_count[cum_sum] = 1
+            else:
+                sum_count[cum_sum] += 1
         
         return ans
