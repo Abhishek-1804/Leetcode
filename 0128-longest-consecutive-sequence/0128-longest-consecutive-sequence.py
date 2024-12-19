ed54fd6bcbc08df6171c9a1c20c0017d27c3f727
Abhishek <50703483+Abhishek-1804@users.noreply.github.com>
2024-12-19 12:45:32 -0700

    Time: 121 ms (43.55%), Space: 40.8 MB (6.89%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,19 +1,17 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
         
-        hmap = {}
-        maxLen = 1
+        h = {}
+        max_num = 0
 
         for num in nums:
-            if num not in hmap:
-                left = hmap.get(num-1, 0)
-                right = hmap.get(num+1, 0)
-                total = 1 + left + right
-                hmap[num-left] = total
-                hmap[num+right] = total
-                hmap[num] = total
-                maxLen = max(maxLen, total)
+            if num not in h:
+                left_length = h.get(num-1, 0)
+                right_length = h.get(num+1, 0)
+                curr_length = left_length + right_length + 1
+                h[num+right_length] = curr_length
+                h[num-left_length] = curr_length
+                h[num] = curr_length
+                max_num = max(max_num, curr_length)
         
-        return maxLen
+        return max_num
