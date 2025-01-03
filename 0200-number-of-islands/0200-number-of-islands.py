a0e793bb7f77d6fe31a26cae9aa6991d21285cbf
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-01-03 15:30:46 -0700

    Time: 249 ms (51.56%), Space: 20.2 MB (46.79%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,22 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        seen = set()
         q = collections.deque([])
         count = 0
 
         for row in range(len(grid)):
             for col in range(len(grid[0])):
-                if grid[row][col] == '1' and (row, col) not in seen:
+                if grid[row][col] == '1':
                     q.append((row, col))
                     while q:
                         r, c = q.popleft()
+                        grid[r][c] = '0'
                         directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
                         for dr, dc in directions:
                             nr, nc = r+dr, c+dc
-                            if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] == '1' and (nr, nc) not in seen:
+                            if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] == '1':
                                 q.append((nr, nc))
-                                seen.add((nr, nc))
+                                grid[nr][nc] = '0'
 
                     count += 1
         
