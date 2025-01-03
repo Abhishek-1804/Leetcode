61896c092e78623c05f50580eea2c73d326f7da9
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-01-03 15:26:37 -0700

    Time: 267 ms (31.79%), Space: 25.1 MB (20.32%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,31 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
-        if not grid:
-            return 0
-
-        visited = set()
-        islands = 0
-
-        def bfs(row, col):
-            queue = deque([(row, col)])
-            while queue:
-                r, c = queue.popleft()
-                if (r, c) not in visited:
-                    visited.add((r, c))
-                    # Check all four directions
-                    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-                    for dr, dc in directions:
-                        nr, nc = r + dr, c + dc
-                        if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and (nr, nc) not in visited and grid[nr][nc] == "1":
-                            queue.append((nr, nc))
+        
+        seen = set()
+        q = collections.deque([])
+        count = 0
 
         for row in range(len(grid)):
             for col in range(len(grid[0])):
-                if grid[row][col] == "1" and (row, col) not in visited:
-                    bfs(row, col)
-                    islands += 1
+                if grid[row][col] == '1' and (row, col) not in seen:
+                    q.append((row, col))
+                    while q:
+                        r, c = q.popleft()
+                        directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
+                        for dr, dc in directions:
+                            nr, nc = r+dr, c+dc
+                            if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] == '1' and (nr, nc) not in seen:
+                                q.append((nr, nc))
+                                seen.add((nr, nc))
+
+                    count += 1
         
-        return islands
-    
-    
+        return count
