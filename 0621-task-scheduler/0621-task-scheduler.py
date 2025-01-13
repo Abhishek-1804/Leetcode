6dc202315f07983709d4ae04d815f4c78cab630f
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-01-14 00:50:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,5 +1,23 @@
 class Solution:
+
     def leastInterval(self, tasks: List[str], n: int) -> int:
+
+        count = Counter(tasks)
+        max_freq = max(count.values())
+        max_count = sum(1 for freq in count.values() if freq == max_freq)
+
+        total_slots = n * (max_freq-1)
+        idle_slots = total_slots - (max_count - 1)
+        remaining_tasks = len(tasks) - (max_freq * max_count)
+        remaining_slots = idle_slots - remaining_tasks
+
+        if remaining_slots > 0:
+            return len(tasks) + remaining_slots
+        
+        else:
+            return len(tasks)
+
+    def leastInterval_1(self, tasks: List[str], n: int) -> int:
         
         count = Counter(tasks)
         maxHeap = [-cnt for cnt in count.values()]
