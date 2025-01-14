18cf9d416700d59050a6af88ff4d6e67e30000e3
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-01-14 05:54:31 +0530

    Time: 18 ms (86.31%), Space: 19.3 MB (19.84%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -6,16 +6,14 @@ class Solution:
         max_freq = max(count.values())
         max_count = sum(1 for freq in count.values() if freq == max_freq)
 
-        total_slots = n * (max_freq-1)
-        idle_slots = total_slots - (max_count - 1)
-        remaining_tasks = len(tasks) - (max_freq * max_count)
-        remaining_slots = idle_slots - remaining_tasks
+        # The core formula:
+        # max_freq-1 * (n+1) is giving us the block structure
+        # so we add max_count again once in the end because the rest of the slots are covered by smaller freq tasks
 
-        if remaining_slots > 0:
-            return len(tasks) + remaining_slots
-        
-        else:
-            return len(tasks)
+        intervals = (max_freq - 1) * (n + 1) + max_count
+
+        # The result can't be smaller than the total number of tasks
+        return max(intervals, len(tasks))
 
     def leastInterval_1(self, tasks: List[str], n: int) -> int:
         
