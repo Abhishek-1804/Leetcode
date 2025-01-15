b642e0d4d9eab51edccf9da855b063cab67e0c4d
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-01-16 02:44:26 +0530

    Time: 4740 ms (36.66%), Space: 18.1 MB (10.35%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -3,29 +3,34 @@ class Solution:
         
         rows = len(board)
         cols = len(board[0])
+        visited = set()
 
-        def dfs(r, c, counter):
-            if counter == len(word):
+        def dfs(row, col, pos):
+            if pos == len(word):
                 return True
             
-            if r < 0 or r >= rows or c < 0 or c >= cols or board[r][c] != word[counter]:
-                return False
-            
-            temp = board[r][c]
-            board[r][c] = '#'
-            
-            found = (dfs(r + 1, c, counter + 1) or
-                     dfs(r - 1, c, counter + 1) or
-                     dfs(r, c + 1, counter + 1) or
-                     dfs(r, c - 1, counter + 1))
-            
-            board[r][c] = temp
+            if (0 <= row < rows and 
+                0 <= col < cols and 
+                (row, col) not in visited and 
+                board[row][col] == word[pos]):
 
-            return found
+            
+                visited.add((row, col))
 
-        for r in range(rows):
-            for c in range(cols):
-                if dfs(r, c, 0):
+                if (dfs(row-1, col, pos + 1) or  # Up
+                    dfs(row+1, col, pos + 1) or  # Down
+                    dfs(row, col-1, pos + 1) or  # Left
+                    dfs(row, col+1, pos + 1)):   # Right
                     return True
-        
+            
+                visited.remove((row, col))
+
+            return False
+            
+        for row in range(rows):
+            for col in range(cols):
+                if word[0] == board[row][col]:
+                    if dfs(row, col, 0):
+                        return True
+                    
         return False
