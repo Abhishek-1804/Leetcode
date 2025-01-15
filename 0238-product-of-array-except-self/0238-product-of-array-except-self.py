bc98bbac0ffde2501a08c3edaa0f2416c1a107ca
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-01-16 04:02:13 +0530

    Time: 11 ms (99.42%), Space: 23.4 MB (47.99%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -8,8 +8,6 @@ class Solution:
             ans.append(curr_prod)
             curr_prod *= i
         
-        print(ans)
-
         curr_prod = 1
         for i in range(len(nums)-1, -1, -1):
             ans[i] *= curr_prod
