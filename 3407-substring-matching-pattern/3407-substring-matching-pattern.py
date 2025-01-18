8b5eab73767e0d67e2835b1f8303fcfde340c649
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-01-19 03:56:39 +0530

    Time: 0 ms (100%), Space: 17.8 MB (43.51%) - LeetHub

--- 3407-substring-matching-pattern/3407-substring-matching-pattern.py
+++ 3407-substring-matching-pattern/3407-substring-matching-pattern.py
@@ -1,5 +1,9 @@
 class Solution:
     def hasMatch(self, s: str, p: str) -> bool:
-        regex_pattern = p.replace('*', '.*')
-        
-        return bool(re.search(regex_pattern, s))
+
+        left_part, right_part = p.split('*')
+
+        left_index = s.find(left_part)
+        right_index = s.find(right_part, left_index + len(left_part))
+
+        return left_index != -1 and right_index != -1
