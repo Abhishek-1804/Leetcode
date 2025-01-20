3634b98bedbe609dbd616466f24f6d5142c72528
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-01-21 03:27:10 +0530

    Time: 2152 ms (8.47%), Space: 18.4 MB (42.97%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        
+        min_exist = [float('inf')] * 26
+        max_exist = [float('-inf')] * 26
+
+        for i in range(len(s)):
+            char_index = ord(s[i]) - ord('a')
+            min_exist[char_index] = min(min_exist[char_index], i)
+            max_exist[char_index] = max(max_exist[char_index], i)
+        
+        ans = set()
+
+        for char_index in range(26):
+            if min_exist[char_index] != float('inf') and max_exist[char_index] != float('-inf'):
+                for j in range(min_exist[char_index] + 1, max_exist[char_index]):
+                    ans.add(s[min_exist[char_index]] + s[j] + s[max_exist[char_index]])
+        
+        return len(ans)
