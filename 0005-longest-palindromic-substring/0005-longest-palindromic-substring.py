83ffb9e5433dd992adfb1cf340eb06e74018f088
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-01-23 00:34:16 +0530

    Time: 308 ms (61.03%), Space: 17.8 MB (59.22%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -2,30 +2,28 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         
         res = ""
-        resLen = 0
+        res_len = 0
 
         for i in range(len(s)):
+
+            left, right = i, i
+
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if (right-left+1) > res_len:
+                    res = s[left:right+1]
+                    res_len = right-left+1
+
+                left -= 1
+                right += 1
+
+            left, right = i, i+1
+
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if (right-left+1) > res_len:
+                    res = s[left:right+1]
+                    res_len = right-left+1
+
+                left -= 1
+                right += 1
             
-            l, r = i, i
-
-            # check for odd length
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r-l+1) > resLen:
-                    res = s[l:r+1]
-                    resLen = r-l+1
-                
-                l -= 1
-                r += 1
-
-            l, r = i, i+1
-
-            #check for even length
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r-l+1) > resLen:
-                    res = s[l:r+1]
-                    resLen = r-l+1
-                
-                l -= 1
-                r += 1
-        
         return res
