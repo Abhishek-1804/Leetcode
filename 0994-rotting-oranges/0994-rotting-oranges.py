e20d9fe8a85358a916159371ca58418156f613ff
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-01-23 00:51:37 +0530

    Time: 5 ms (38.3%), Space: 17.9 MB (28.48%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,31 +1,31 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-
-        rows, cols = len(grid), len(grid[0])
-        q = deque([])
-        fresh_oranges = 0
-
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 1:
-                    fresh_oranges += 1
-                elif grid[r][c] == 2:
-                    q.append((r, c))
         
-        minutes = 0
-        directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+        rows, cols = len(grid), len(grid[0])
+
+        fresh_oranges = 0
+        q = deque([])
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 1:
+                    fresh_oranges += 1
+                elif grid[row][col] == 2:
+                    q.append((row, col))
+
+        ans = 0
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
         while q and fresh_oranges > 0:
             for _ in range(len(q)):
-                r, c = q.popleft()
+                row, col = q.popleft()
                 for dr, dc in directions:
-                    nr, nc = r + dr, c + dc
-                    # If adjacent orange is fresh, rot it and add to the queue
+                    nr, nc = dr + row, dc + col
                     if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+                        fresh_oranges -= 1
                         grid[nr][nc] = 2
                         q.append((nr, nc))
-                        fresh_oranges -= 1
             
-            minutes += 1
+            ans += 1
         
-        return minutes if fresh_oranges == 0 else -1
+        return ans if fresh_oranges == 0 else -1
