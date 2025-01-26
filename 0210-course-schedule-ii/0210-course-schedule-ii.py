c1dcc8be139c2b0738c0e2a5b8850f0248cd1b2c
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-01-27 00:56:56 +0530

    Time: 3 ms (78.67%), Space: 19.6 MB (24.04%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,33 +1,60 @@
 class Solution:
-    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        
-        adj_list = {i: [] for i in range(numCourses)}
+    # bfs
+    def findOrder_1(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+
+        pre_to_crs = {i:[] for i in range(numCourses)}
+        dependencies = [0] * numCourses
 
         for crs, pre in prerequisites:
-            adj_list[crs].append(pre)
-        
-        output = []
-        # 0 unvisited, 1 visiting, 2 visited
-        state = [0] * numCourses
+            pre_to_crs[pre].append(crs)
+            dependencies[crs] += 1
 
-        def hasCycle(i):
-            if state[i] == 1:
+        q = deque([i for i in range(numCourses) if dependencies[i] == 0])
+        result = []
+
+        while q:
+            crs = q.popleft()
+            result.append(crs)
+
+            for neighbor in pre_to_crs[crs]:
+                dependencies[neighbor] -= 1
+                if dependencies[neighbor] == 0:
+                    q.append(neighbor)
+        
+        return result if len(result) == numCourses else []
+
+    # dfs
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        crs_to_pre = {i:[] for i in range(numCourses)}
+
+        for crs, pre in prerequisites:
+            crs_to_pre[crs].append(pre)
+        
+        # 0 - not visited
+        # 1 - currently visiting
+        # 2 - visited
+        states = [0]*numCourses
+        result = []
+
+        def has_cycle(i):
+            if states[i] == 1:
                 return True
-            
-            if state[i] == 2:
+            if states[i] == 2:
                 return False
-            
-            state[i] = 1
-            for neighbor in adj_list[i]:
-                if hasCycle(neighbor):
+
+            states[i] = 1
+            for crs in crs_to_pre[i]:
+                if has_cycle(crs):
                     return True
-            
-            state[i] = 2
-            output.append(i)
+            states[i] = 2
+
+            result.append(i)
             return False
-        
-        for i in adj_list:
-            if hasCycle(i):
-                return []
-        
-        return output
+
+        for i in range(numCourses):
+            if states[i] == 0:
+                if has_cycle(i):
+                    return []
+
+        return result
