bf092af6d5813f86b4362136faa70d6942f03855
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-01-30 00:24:41 +0530

    Time: 7 ms (83.64%), Space: 18.3 MB (40.77%) - LeetHub

--- 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
@@ -2,29 +2,27 @@ class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
         
         webInfo = []
-
         for u, t, w in zip(username, timestamp, website):
             webInfo.append((u, t, w))
         
-        webInfo.sort(key=lambda x: x[1])
+        webInfo.sort(key=lambda x : x[1])
 
-        websiteVisit = defaultdict(list)
-        for u, t, w in webInfo:
-            websiteVisit[u].append(w)
+        visits = defaultdict(list)
+        for u, _, w in webInfo:
+            visits[u].append(w)
         
         possibleTuples = defaultdict(int)
-        for usr in websiteVisit:
-            webRoutes = set(combinations(websiteVisit[usr], 3))
+        for u in visits:
+            webRoutes = set(combinations(visits[u], 3))
             for webRoute in webRoutes:
                 possibleTuples[webRoute] += 1
-
+        
         maxVal, routes = max(possibleTuples.values()), []
         for r, val in possibleTuples.items():
-            if val == maxVal:
+            if maxVal == val:
                 routes.append(r)
-                
+        
         if len(routes) > 1:
-            # SORTS LEXICOGRAPHICALLY
             routes.sort()
         
         return routes[0]
