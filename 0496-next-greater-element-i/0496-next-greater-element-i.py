8d94d7d22beac66e0d25dabd76fae71a73aeb1d6
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-02-11 00:38:27 +0530

    Time: 0 ms (100%), Space: 18 MB (60.26%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,5 +1,24 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
+        # Dictionary to store the next greater element for each number in nums2
+        next_greater = {}
+        stack = []
+        
+        # Iterate over nums2 and build the next greater mapping
+        for num in nums2:
+            while stack and num > stack[-1]:
+                next_greater[stack.pop()] = num
+            stack.append(num)
+        
+        # For any remaining elements in the stack, there is no next greater element.
+        while stack:
+            next_greater[stack.pop()] = -1
+        
+        # Build the result for nums1 using the precomputed mapping
+        return [next_greater[num] for num in nums1]
+
+    def nextGreaterElement_bf(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
         ans = []
         n = len(nums1)
@@ -16,5 +35,4 @@ class Solution:
             if flag:
                 ans.append(-1)
 
-            
         return ans
