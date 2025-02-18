a26ace0ca1e496980892cf428798d173b7475fb7
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-02-18 23:57:08 +0530

    Time: 0 ms (100%), Space: 18.4 MB (25.23%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,33 +1,31 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-
-        row_low = 0
-        row_high = len(matrix) - 1
-        col_low = 0
-        col_high = len(matrix[0]) - 1
-
-        while row_low < row_high:
-            row_mid = (row_low + row_high) // 2
         
-            if target >= matrix[row_mid][0] and target <= matrix[row_mid][-1]:
-                # The target is within the range of this row
-                row_low = row_mid
+        start = 0
+        end = len(matrix)-1
+
+        while start <= end:
+            mid = (start+end) // 2
+
+            if matrix[mid][0] <= target <= matrix[mid][-1]:
                 break
-            elif target < matrix[row_mid][0]:
-                row_high = row_mid - 1
+            elif target > matrix[mid][-1]:
+                start = mid + 1
             else:
-                row_low = row_mid + 1
+                end = mid - 1
         
-        while col_low <= col_high:
-            col_mid = (col_low + col_high) // 2
-            
-            if target == matrix[row_low][col_mid]:
+        row = mid
+        start = 0
+        end = len(matrix[0]) - 1
+
+        while start <= end:
+            mid = (start + end) // 2
+
+            if target == matrix[row][mid]:
                 return True
-            
-            elif target > matrix[row_low][col_mid]:
-                col_low = col_mid + 1
-            
+            elif target < matrix[row][mid]:
+                end = mid - 1
             else:
-                col_high = col_mid - 1
+                start = mid + 1
         
         return False
