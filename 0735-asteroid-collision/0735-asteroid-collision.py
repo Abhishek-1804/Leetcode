2459a76f72bcff5aca1f3dd8f118d083604a8263
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-02-24 06:29:36 +0530

    Time: 9 ms (22.84%), Space: 19.1 MB (19.14%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,8 +1,11 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+
         stack = []
+
         for a in asteroids:
             flag = False
+            
             while stack and a < 0 and stack[-1] > 0:
                 if stack[-1] < abs(a):
                     stack.pop()
@@ -14,5 +17,5 @@ class Solution:
 
             if not flag:
                 stack.append(a)
-                
+
         return stack
