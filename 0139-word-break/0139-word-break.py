c10afaf7dbbf38c82ee2b2429b292557a68dc465
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-02-25 11:18:06 -0700

    Time: 3 ms (69.11%), Space: 17.8 MB (87.43%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,12 +1,12 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * (len(s) + 1)
+        dp[0] = True  # Empty string can be segmented
         
-        dp = [False] * (len(s)+1)
-        dp[0] = True
-
-        for i in range(1, len(s)+1):
+        for i in range(1, len(s) + 1):
             for j in range(i):
-                if dp[j] and (s[j:i] in wordDict):
+                if dp[j] and s[j:i] in wordDict:
                     dp[i] = True
+                    break
         
-        return dp[-1]
+        return dp[len(s)]
