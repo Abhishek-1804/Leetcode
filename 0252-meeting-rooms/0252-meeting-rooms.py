9e396135864f578d06401f36c6773f5f859e31fd
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-09-21 09:12:18 -0700

    Time: 4 ms (30.83%), Space: 20 MB (16.57%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -3,14 +3,12 @@ class Solution:
 
         if not intervals:
             return True
-
-       intervals.sort(key = lambda x: x[0]) 
-
-       prev = intervals[0][1]
-
-       for i in intervals[1:]:
-        if prev > i[0]:
-            return False
-        prev = i[1]
-
+        intervals.sort(key = lambda x: x[0]) 
+        prev = intervals[0][1]
+        
+        for i in intervals[1:]:
+            if prev > i[0]:
+                return False
+            prev = i[1]
+        
         return True
