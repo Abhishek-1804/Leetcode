878446d0d2f9260128ced9ca7ef43d8c297adedd
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-09-25 13:05:44 -0700

    Time: 14 ms (34.77%), Space: 20.9 MB (42.24%) - LeetHub

---
+++ 1214-two-sum-bsts/1214-two-sum-bsts.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def twoSumBSTs(self, root1: Optional[TreeNode], root2: Optional[TreeNode], target: int) -> bool:
+        def search(root, val):
+            if not root:
+                return False
+            if root.val == val:
+                return True
+            elif val < root.val:
+                return search(root.left, val)
+            else:
+                return search(root.right, val)
+        
+        def traverse(root1):
+            if not root1:
+                return False
+            # search for complement in root2
+            if search(root2, target - root1.val):
+                return True
+            # traverse both subtrees
+            return traverse(root1.left) or traverse(root1.right)
+
+        return traverse(root1)
