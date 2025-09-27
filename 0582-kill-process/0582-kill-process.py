1786105c71e5f05abf483a058cffe789f1266dcc
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-09-27 10:01:59 -0700

    Time: 20 ms (95.88%), Space: 22.4 MB (83.15%) - LeetHub

---
+++ 0582-kill-process/0582-kill-process.py
@@ -0,0 +1,20 @@
+class Solution:
+    def killProcess(self, pid: List[int], ppid: List[int], kill: int) -> List[int]:
+        from collections import deque, defaultdict
+
+        parent_to_child = defaultdict(list)
+        output = []
+
+        for i in range(len(ppid)):
+            parent_to_child[ppid[i]].append(pid[i])
+        
+        bfs_q = deque([kill])
+
+        while bfs_q:
+            parent = bfs_q.popleft()
+            output.append(parent)
+            if parent in parent_to_child:
+                for child in parent_to_child[parent]:
+                    bfs_q.append(child)
+        
+        return output
