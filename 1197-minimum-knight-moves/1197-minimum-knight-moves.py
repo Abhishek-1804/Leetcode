feb54e626d2f9121f019081ab8e280d494c62ede
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-09-29 12:20:43 -0700

    Time: 18 ms (91.4%), Space: 19.1 MB (88.51%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.py
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -1,38 +1,14 @@
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
-        x, y = abs(x), abs(y)
-        if (x < y): x, y = y, x
-        if (x == 1 and y == 0): return 3        
-        if (x == 2 and y == 2): return 4 
-        delta = x - y
-        if (y > delta): return delta - 2 * int((delta - y) // 3)
-        else: return delta - 2 * int((delta - y) // 4)
+        @lru_cache(maxsize=None)
+        def dfs(x, y):
+            if x + y == 0:
+                # base case: (0, 0)
+                return 0
+            elif x + y == 2:
+                # base case: (1, 1), (0, 2), (2, 0)
+                return 2
+            else:
+                return min(dfs(abs(x - 1), abs(y - 2)), dfs(abs(x - 2), abs(y - 1))) + 1
 
-    def minKnightMoves_bfs(self, x: int, y: int) -> int:
-        # the offsets in the eight directions
-        offsets = [(1, 2), (2, 1), (2, -1), (1, -2),
-                   (-1, -2), (-2, -1), (-2, 1), (-1, 2)]
-
-        def bfs(x, y):
-            visited = set()
-            queue = deque([(0, 0)])
-            steps = 0
-
-            while queue:
-                curr_level_cnt = len(queue)
-                # iterate through the current level
-                for i in range(curr_level_cnt):
-                    curr_x, curr_y = queue.popleft()
-                    if (curr_x, curr_y) == (x, y):
-                        return steps
-
-                    for offset_x, offset_y in offsets:
-                        next_x, next_y = curr_x + offset_x, curr_y + offset_y
-                        if (next_x, next_y) not in visited:
-                            visited.add((next_x, next_y))
-                            queue.append((next_x, next_y))
-
-                # move on to the next level
-                steps += 1
-
-        return bfs(x, y)
+        return dfs(abs(x), abs(y))
