b69b4fd8a13fcb37763b229dafb3bce9aecedf89
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-09-29 12:12:07 -0700

    Time: 0 ms (100%), Space: 17.7 MB (99.78%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.py
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -1,5 +1,14 @@
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
+        x, y = abs(x), abs(y)
+        if (x < y): x, y = y, x
+        if (x == 1 and y == 0): return 3        
+        if (x == 2 and y == 2): return 4 
+        delta = x - y
+        if (y > delta): return delta - 2 * int((delta - y) // 3)
+        else: return delta - 2 * int((delta - y) // 4)
+
+    def minKnightMoves_bfs(self, x: int, y: int) -> int:
         # the offsets in the eight directions
         offsets = [(1, 2), (2, 1), (2, -1), (1, -2),
                    (-1, -2), (-2, -1), (-2, 1), (-1, 2)]
