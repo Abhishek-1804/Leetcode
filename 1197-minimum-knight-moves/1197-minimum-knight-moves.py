809e562f7028bb4292ba8a84155300c4843b1031
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-09-29 12:22:36 -0700

    Time: 14 ms (96.46%), Space: 19 MB (93.93%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.py
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -1,5 +1,39 @@
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
+        # return self.minKnightMovesbfs(x, y)
+        return self.minKnightMoves_dfs(x, y)
+
+
+    def minKnightMovesbfs(self, x: int, y: int) -> int:
+        # the offsets in the eight directions
+        offsets = [(1, 2), (2, 1), (2, -1), (1, -2),
+                   (-1, -2), (-2, -1), (-2, 1), (-1, 2)]
+        
+        def bfs(x, y):
+            visited = set()
+            queue = deque([(0, 0)])
+            steps = 0
+
+            while queue:
+                curr_level_cnt = len(queue)
+                # iterate through the current level
+                for i in range(curr_level_cnt):
+                    curr_x, curr_y = queue.popleft()
+                    if (curr_x, curr_y) == (x, y):
+                        return steps
+
+                    for offset_x, offset_y in offsets:
+                        next_x, next_y = curr_x + offset_x, curr_y + offset_y
+                        if (next_x, next_y) not in visited:
+                            visited.add((next_x, next_y))
+                            queue.append((next_x, next_y))
+
+                # move on to the next level
+                steps += 1
+
+        return bfs(x, y)
+    
+    def minKnightMoves_dfs(self, x: int, y: int) -> int:
         @lru_cache(maxsize=None)
         def dfs(x, y):
             if x + y == 0:
