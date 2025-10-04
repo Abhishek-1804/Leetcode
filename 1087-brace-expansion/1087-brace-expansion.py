c6bff5a2aeb13c3287f6ace95a983f46fe6a7750
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-10-03 19:37:53 -0700

    Time: 2 ms (51.68%), Space: 18.2 MB (42.54%) - LeetHub

---
+++ 1087-brace-expansion/1087-brace-expansion.py
@@ -0,0 +1,23 @@
+class Solution:
+    def expand(self, s: str) -> List[str]:
+
+        output = []
+
+        def backtrack(ind, temp_str):
+            if ind == len(s):
+                output.append(temp_str)
+                return
+            
+            if s[ind] == '{':
+                end = s.index('}', ind)
+
+                options = s[ind+1:end].split(',')
+
+                for op in options:
+                    backtrack(end + 1, temp_str+op)
+            else:
+                backtrack(ind+1, temp_str+s[ind])
+
+        backtrack(0, "")
+
+        return sorted(output)
