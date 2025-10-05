71c5aa01c2511cb1c0171a67b922795a91bce75a
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-10-05 14:01:12 -0700

    Time: 2917 ms (89.43%), Space: 18 MB (21.96%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,32 +1,31 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
 
-        def dfs(row, col, i):
-            if i == len(word):
+        rows, cols = len(board), len(board[0])
+        word_length = len(word)
+
+        def backtrack(row, col, ind):
+
+            if ind == word_length:
                 return True
             
-            if 0 <= row < rows and 0 <= col < cols and (row, col) not in visited and board[row][col] == word[i]:
-                visited.add((row, col))
+            if row < 0 or row >= rows or col < 0 or col >= cols or board[row][col] != word[ind]:
+                return False
 
-                if (
-                    dfs(row+1, col, i+1) or 
-                    dfs(row-1, col, i+1) or 
-                    dfs(row, col+1, i+1) or 
-                    dfs(row, col-1, i+1)
-                ):
-                    return True
+            temp = board[row][col]
+            board[row][col] = '#'
+            if (backtrack( row+1, col, ind+1 ) or 
+                backtrack( row-1, col, ind+1 ) or
+                backtrack( row, col+1, ind+1 ) or
+                backtrack( row, col-1, ind+1 )):
+                return True
 
-                visited.remove((row, col))
+            board[row][col] = temp
             
-            return False
-        
-        rows, cols = len(board), len(board[0])
-        visited = set()
-
         for row in range(rows):
             for col in range(cols):
                 if board[row][col] == word[0]:
-                    if dfs(row, col, 0):
+                    if backtrack(row, col, 0):
                         return True
         
         return False
