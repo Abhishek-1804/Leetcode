70c28bf292f98349f3c3143385cc37ea4724a605
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-10-30 10:48:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,32 +1,10 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
 
-        states = [0] * 26
+        from collections import Counter
 
-        for i in s:
-            # for capital letters
-            if 65 <= ord(i) <= 90:
-                if states[ord(i)-65] == 0:
-                    states[ord(i)-65] = 1
-                elif states[ord(i)-65] == 1:
-                    states[ord(i)-65] = 2
-                else:
-                    continue
-            
-            else:
-                if states[ord(i)-97] == 0:
-                    states[ord(i)-97] = 1
-                elif states[ord(i)-97] == 1:
-                    states[ord(i)-97] = 2
-                else:
-                    continue
-        
+        counts = Counter(s)
+
         for i in range(len(s)):
-            if 65 <= ord(s[i]) <= 90:
-                if states[ord(s[i]) - 65] == 1:
-                    return i
-            else:
-                if states[ord(s[i]) - 97] == 1:
-                    return i
-        
-        return -1
+            if counts[s[i]] == 1:
+                return i
