980dc0637248960f0e95b650f30c3aa5138c7e9b
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-10-31 09:45:39 +0530

    Time: 3 ms (81.63%), Space: 17.9 MB (44.08%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,22 +1,25 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
+        rows, cols = len(board), len(board[0])
+
         row_set = set()
         col_set = set()
         box_set = set()
-
-        rows, cols = len(board), len(board[0])
 
         for row in range(rows):
             for col in range(cols):
                 if board[row][col] == '.':
                     continue
+                    
+                row_check = (board[row][col], row)
+                col_check = (board[row][col], col)
+                box_check = (board[row][col], row//3, col//3)
 
-                if (board[row][col], row) in row_set or (board[row][col], col) in col_set or (board[row][col], (row//3, col//3)) in box_set:
+                if row_check in row_set or col_check in col_set or box_check in box_set:
                     return False
-                
-                row_set.add((board[row][col], row))
-                col_set.add((board[row][col], col))
-                box_set.add((board[row][col], (row//3, col//3)))
+                row_set.add(row_check)
+                col_set.add(col_check)
+                box_set.add(box_check)
         
         return True
