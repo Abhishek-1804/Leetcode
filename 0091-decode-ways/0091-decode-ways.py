cbba3d8c4a12218938abed960f04a7505bdc236b
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-10-31 11:38:40 +0530

    Time: 0 ms (100%), Space: 18 MB (15.57%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,5 +1,38 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        # return self.numDecodings_1(s)
+        return self.numDecodings_2(s)
+
+    def numDecodings_2(self, s: str) -> int:
+    
+        dp = [0 for i in range(len(s) + 1)]
+
+        dp[0] = 1
+
+        if s[0] == '0':
+            return 0
+
+        else:
+            dp[1] = 1
+
+        for i in range(2, len(s)+1):
+            if s[i-1] == '0':
+                if s[i-2] == '1' or s[i-2] == '2':
+                    dp[i] = dp[i-2]
+
+                else:
+                    return 0
+
+            else:
+                if (s[i-2] == '1') or (s[i-2] == '2' and int(s[i-1]) < 7):
+                    dp[i] = dp[i-1] + dp[i-2]
+
+                else:
+                    dp[i] = dp[i-1]
+
+        return dp[len(s)]
+
+    def numDecodings_1(self, s: str) -> int:
         
         dp = {len(s): 1}
 
