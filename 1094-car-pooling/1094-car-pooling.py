49e0d440b6d34e984daba2cf281faf3fe78ef178
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-11-04 15:39:05 +0530

    Time: 42 ms (7.39%), Space: 18.2 MB (53.6%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,16 +1,18 @@
-from collections import deque
-from typing import List
-
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        trips.sort(key = lambda x: x[1])  # sort by start time
-        q = deque([])
-        total_people = 0
-        for people, start, end in trips:
-            while q and start >= q[0][1]:
-                total_people -= q.popleft()[0]
-            if people + total_people > capacity:
+        
+        diff = [0] * 1001
+
+        for num_pass, start, end in trips:
+            diff[start] += num_pass
+            diff[end] -= num_pass
+
+        passengers = 0
+
+        for x in diff:
+            passengers += x
+            if passengers > capacity:
                 return False
-            total_people += people
-            q.append((people, end))
+                
         return True
+   
