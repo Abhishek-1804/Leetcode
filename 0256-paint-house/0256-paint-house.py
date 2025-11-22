a20235d2f2e9a8a97f274c9efa1a4fe183197bf2
Abhishek Deshpande <50703483+Abhishek-1804@users.noreply.github.com>
2025-11-22 12:37:42 +0530

    Time: 2 ms (74.65%), Space: 17.6 MB (97.1%) - LeetHub

--- 0256-paint-house/0256-paint-house.py
+++ 0256-paint-house/0256-paint-house.py
@@ -1,13 +1,13 @@
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
-
         n = len(costs)
-
-        if(n==0) : return 0
-
-        for i in range(1,n):
-            costs[i][0] += min(costs[i-1][1],costs[i-1][2])
-            costs[i][1] += min(costs[i-1][0],costs[i-1][2])            
-            costs[i][2] += min(costs[i-1][1],costs[i-1][0])            
-            
-        return min(costs[-1])
+        
+        # Create a proper copy or use costs directly
+        dp = [row[:] for row in costs]  # Deep copy
+        
+        for i in range(1, n):
+            dp[i][0] = costs[i][0] + min(dp[i-1][1], dp[i-1][2])
+            dp[i][1] = costs[i][1] + min(dp[i-1][0], dp[i-1][2])
+            dp[i][2] = costs[i][2] + min(dp[i-1][0], dp[i-1][1])
+        
+        return min(dp[n-1])
